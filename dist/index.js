5f593a021cd9d24840a134d4bd585ba0f6ffad63
danas <jackunasd231@gmail.com>
2024-09-16 18:17:05 +0300

    .

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from '/three/examples/jsm/Addons.js';
+import { GLTFLoader } from '/three/addons/loaders/GLTFLoader.js';
 
 class PickHelper {
 	constructor() {
